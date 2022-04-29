587d536e151de973e2a2ae4a9b30744502d31c97
Marko Damjanac <36675797+damlern92@users.noreply.github.com>
2022-04-29 13:22:58 +0200

    Update config.php

--- config.php
+++ config.php
@@ -4,4 +4,4 @@ define("DBUSER","root");
 define("DBPASS","");
 define("DBNAME","cars");
 
-$conn = mysqli_connect(DBHOST,DBUSER,DBPASS,DBNAME);
+$conn = @mysqli_connect(DBHOST,DBUSER,DBPASS,DBNAME);
